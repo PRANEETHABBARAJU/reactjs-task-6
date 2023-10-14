afbf5031aeddbae14d9375fdd64f519a9222f77a
PraneethAbbaraju <praneeth.abbaraju22@gmail.com>
2023-10-14 22:52:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; 
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="portfolio">
+      <header>
+        <h1>Praneeth Abbaraju</h1>
+        <p>Web Developer</p>
       </header>
+      <section className="projects">
+        <h2>Projects</h2>
+        
+      </section>
+      <section className="skills">
+        <h2>Skills</h2>
+        
+      </section>
+      <section className="experience">
+        <h2>Experience</h2>
+        
+      </section>
+      <section className="education">
+        <h2>Education</h2>
+        git
+      </section>
+      <section className="contact">
+        <h2>Contact</h2>
+        {/* Add your contact information */}
+      </section>
     </div>
   );
-}
+};
 
 export default App;
